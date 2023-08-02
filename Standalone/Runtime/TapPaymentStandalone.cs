0cadd92069135c816cff5b23ba9b104683789bcb
ci-gitlab <tds-developer@xd.com>
2023-08-02 14:11:53 +0800

    feat:update upm

---
+++ Standalone/Runtime/TapPaymentStandalone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TapTap.Common;
+using TapTap.Payment.Internal;
+
+namespace TapTap.Payment.Standalone {
+    public class TapPaymentStandalone : ITapPaymentBridge {
+        public void Init(TapConfig config) {
+            TapLogger.Warn($"{nameof(Init)} NOT implemented.");
+        }
+
+        public bool IsReady() {
+            TapLogger.Warn($"{nameof(IsReady)} NOT implemented.");
+            return false;
+        }
+
+        public void LaunchBillingFlow(SkuDetails skuDetails, string roleId, string serverId, string extra, Action<int, TapError> action) {
+            TapLogger.Warn($"{nameof(LaunchBillingFlow)} NOT implemented.");
+        }
+
+        public void QueryProduct(string skuId, Action<SkuDetails, TapError> action) {
+            TapLogger.Warn($"{nameof(QueryProduct)} NOT implemented.");
+        }
+
+        public void QueryProducts(string[] skuIds, Action<List<SkuDetails>, TapError> action) {
+            TapLogger.Warn($"{nameof(QueryProducts)} NOT implemented.");
+        }
+
+        public void SwitchLanguage(string languageId) {
+            TapLogger.Warn($"{nameof(SwitchLanguage)} NOT implemented.");
+        }
+    }
+}
